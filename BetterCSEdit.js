a78bb91aa59d084f987a5275841a6034afc89cf1
Bang1338 <75790567+Bang1338@users.noreply.github.com>
2022-10-09 20:01:10 +0700

    1.3.1 BABEEEEEEEEEEEEEEEE

--- BetterCSEdit.js
+++ BetterCSEdit.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         BetterCSEdit
 // @namespace    BCSE
-// @version      1.2.1
+// @version      1.3.1
 // @description  A better editing for Comic Studio!
 // @author       bang1338
 // @match        *://*.comic.studio
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=comic.studio
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 // ==Changelog==
@@ -17,47 +18,82 @@
 // 1.1   - Change (max) and (min) to + and - for 100% zoom
 // 1.2   - Removed maxlength = '3' because it's doesn't working anymore
 // 1.2.1 - Shorten "Rotate" and "Resize" words to "Ro" and "Re" so it will fit some font
+// 1.3.1 - Added title bypass for clentside and Adding Enable/Disable option, powered by boolean :)
 // ==Changelog==
 
 // This is Open Source.
-
 (function()
 {
     'use strict';
+    // 1 = Enable | 0 = Disable
+    // renro: Resize and Rotate
+    // tgtb | Title length bypass (clientside)
 
-    let resmin = document.createElement('input')
-    resmin.type = 'text'
-    resmin.placeholder='Re-'
-    document.getElementById('controls').prepend(resmin)
-    resmin.oninput = function()
+    // ======= [EDIT-ABLE] =======
+    let renro = 1;
+    let tgtb = 1;
+    // ======= [EDIT-ABLE] =======
+
+
+
+    // ======= [DONT EDIT] =======
+    // or edit if you know what are you doing
+
+    // Resize and Rotate input function
+    if (Boolean(renro) == true)
     {
-    document.getElementById('resize').min = this.value;
+        // Resize input (decrease)
+        let resmin = document.createElement('input')
+        resmin.type = 'text'
+        resmin.placeholder = 'Re-'
+        document.getElementById('controls').prepend(resmin)
+        resmin.oninput = function()
+        {
+            document.getElementById('resize').min = this.value;
+        }
+
+        // Resize input (increase)
+        let resmax = document.createElement('input')
+        resmax.type = 'text'
+        resmax.placeholder = 'Re+'
+        document.getElementById('controls').prepend(resmax)
+        resmax.oninput = function()
+        {
+            document.getElementById('resize').max = this.value;
+        }
+
+        // Rotate input (decrease)
+        let rotmin = document.createElement('input')
+        rotmin.type = 'text'
+        rotmin.placeholder = 'Ro-'
+        document.getElementById('controls').appendChild(rotmin)
+        rotmin.oninput = function()
+        {
+            document.getElementById('rotate').min = this.value;
+        }
+
+        // Rotate input (increase)
+        let rotmax = document.createElement('input')
+        rotmax.type = 'text'
+        rotmax.placeholder = 'Ro+'
+        document.getElementById('controls').appendChild(rotmax)
+        rotmax.oninput = function()
+        {
+            document.getElementById('rotate').max = this.value;
+        }
     }
 
-    let resmax = document.createElement('input')
-    resmax.type = 'text'
-    resmax.placeholder='Re+'
-    document.getElementById('controls').prepend(resmax)
-    resmax.oninput = function()
-    {
-    document.getElementById('resize').max = this.value;
-    }
 
-    let rotmin = document.createElement('input')
-    rotmin.type = 'text'
-    rotmin.placeholder='Ro-'
-    document.getElementById('controls').appendChild(rotmin)
-    rotmin.oninput = function()
+    // Title bypass (clentside) function
+    if (Boolean(tgtb) == true)
     {
-    document.getElementById('rotate').min = this.value;
-    }
-
-    let rotmax = document.createElement('input')
-    rotmax.type = 'text'
-    rotmax.placeholder='Ro+'
-    document.getElementById('controls').appendChild(rotmax)
-    rotmax.oninput = function()
-    {
-    document.getElementById('rotate').max = this.value;
+        let tb = document.createElement('input')
+        tb.type = 'text'
+        tb.placeholder = 'Title length (client if >50)'
+        document.getElementsByClassName("right-buttons tab-buttons")[0].appendChild(tb)
+        tb.oninput = function()
+        {
+            document.getElementById('title').maxLength = this.value;
+        }
     }
 })();
