bda6aa77fc88a19362b64a380ca5e86e4b220a22
Bang1338 <75790567+Bang1338@users.noreply.github.com>
2022-11-04 14:45:11 +0700

    1.4, hope it working.

--- BetterCSEdit.js
+++ BetterCSEdit.js
@@ -1,14 +1,25 @@
 // ==UserScript==
 // @name         BetterCSEdit
 // @namespace    BCSE
-// @version      1.3.1
+// @version      1.4
 // @description  A better editing for Comic Studio!
 // @author       bang1338
-// @match        *://*.comic.studio
+// @match        *://*.comic.studio/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=comic.studio
 // @grant        none
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @license      MIT
 // ==/UserScript==
+
+// === Inspired from: ===
+// BetterEdit: https://github.com/HJfod/BetterEdit
+// BetterDiscord: https://betterdiscord.app/
+// Mega Hack Pro: https://absolllute.com/store/view_mega_hack_pro
+// === End of inspr ===
+
+// === Powered by: ===
+// jQuery.
+// === End powered ===
 
 // ==Changelog==
 // 0.1   - First script, which you have to open the script to change max size.
@@ -19,25 +30,90 @@
 // 1.2   - Removed maxlength = '3' because it's doesn't working anymore
 // 1.2.1 - Shorten "Rotate" and "Resize" words to "Ro" and "Re" so it will fit some font
 // 1.3.1 - Added title bypass for clentside and Added Enable/Disable option, powered by boolean :)
+// 1.4   - Added Dark Mode and Custom Welcome.
 // ==Changelog==
 
 // This is Open Source.
 (function()
 {
+
     'use strict';
-    // 1 = Enable | 0 = Disable
-    // renro: Resize and Rotate
-    // tgtb | Title length bypass (clientside)
 
     // ======= [EDIT-ABLE] =======
+
+    // 1 = Enable | 0 = Disable
+    // =====[Common Setting] =====
+    // renro: Resize and Rotate
+    // tgtb: Title length bypass (clientside)
+    // darkmode: Dark mode.
+    // customwelcome: Custom Welcome
     let renro = 1;
     let tgtb = 1;
-    // ======= [EDIT-ABLE] =======
+    let darkmode = 1;
+    let customwelcome = 1;
+    // =====[/Common Setting] =====
+
+
+    // =====[Custom Welcome] =====
+    // fw: First word | lw: Last word
+    // inclnm: Include name | entstr: Entire string
+    let inclnm = 1;
+    let fw = "Welcome back, ";
+    let lw = ", enjoy your stay!";
+    let entstr = "Hello there!"
+    // Note: DO NOT PUT "
+    // Note: If inclnm is 0, use entstr
+    // =====[/Custom Welcome] =====
+
+
+    // =====[Dark-mode] =====
+    // rfs = Refresh speed | 500 is recommend.
+    let rfs = 500;
+    // =====[/Dark-mode] =====
+
+
+    // ======= [/EDIT-ABLE] =======
 
 
 
     // ======= [DONT EDIT] =======
     // or edit if you know what are you doing
+
+
+    // Custom Welcome function
+    if ( Boolean(customwelcome) == true )
+    {
+        var rescw = "";
+        var blank = "";
+        var text = document.getElementsByClassName('display-5 fw-bold lh-1 mb-3')[0].textContent;
+        var nameonl = text.replace('Welcome, ','');
+
+        if ( Boolean(inclnm) == true ) rescw = blank.concat(fw, nameonl, lw);
+        else rescw = entstr;
+
+        document.getElementsByClassName('display-5 fw-bold lh-1 mb-3')[0].innerHTML = rescw;
+    }
+
+
+    // Dark mode function
+    if ( Boolean(darkmode) == true )
+    {
+        // Dark mode, run.
+        setInterval(function()
+        {
+            // darkmode main.
+            // Font will turn into white to read in dark.
+            $('#container').attr('style','background: #000')
+            $('.card').attr('style','background-color: #000')
+            $('.comics-box').attr('style','background-color: #000')
+            $('.notification-inner a').attr('style','color: #6c757d')
+            $('.table').attr('style','color: #fff')
+            $('.text-body').attr('style','color: rgba(var(--bs-white),var(--bs-text-opacity)) !important')
+            $('.text-black').attr('style','color: rgba(var(--bs-white),var(--bs-text-opacity)) !important')
+            document.getElementsByClassName("w-100")[0].src = "https://cdn.discordapp.com/attachments/954077931360124939/1028601397337399376/icon_black.png" // Adding edited icon so you can see it.
+            document.body.style.color='#ffffff';
+        }, rfs);
+    }
 
     // Resize and Rotate input function
     if (Boolean(renro) == true)
@@ -96,4 +172,7 @@
             document.getElementById('title').maxLength = this.value;
         }
     }
+
+
+    // ======= [/DONT EDIT] =======
 })();
